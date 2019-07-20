f5972bdc3c776e1621a6050a1ac22be2d064d988
Jerry L Hoover <jerry.hoover@ymail.com>
2019-07-19 20:55:06 -0500

    19 July Commit

---
+++ js/app.js
@@ -0,0 +1,11 @@
+function getUsers() {
+   // dummy
+}
+
+function main() {
+   // This comment serves no purpose
+   let disclaimer = "This is the JavaScript for Treehouse Project 8";
+   console.log(disclaimer);
+}
+
+main();
