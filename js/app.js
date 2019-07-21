c6fbd947dffc1b37ef7d1484bf0d2f4481db91ca
Jerry L Hoover <jerry.hoover@ymail.com>
2019-07-20 21:54:04 -0500

    Styling Complete(ish)

--- js/app.js
+++ js/app.js
@@ -7,53 +7,76 @@ function showUsers() {
          last_name: "Chambers",
          email: "victoria.chambers80@example.com",
          city: "Springfield"
+      },
+      {
+         image: "images/member-1.jpg",
+         first_name: "Victoria",
+         last_name: "Chambers",
+         email: "victoria.chambers80@example.com",
+         city: "Springfield"
+      },
+      {
+         image: "images/member-1.jpg",
+         first_name: "Victoria",
+         last_name: "Chambers",
+         email: "victoria.chambers80@example.com",
+         city: "Springfield"
       }
    ];
-   // TODO
-   // bring JSON data into this function
+   // TODO this is important
+   // bring JSON data into this function (pass JSON object to func)
    // Loop thru data, creating one card for each employee
-   // STYLE so info aligns with image
-   // shorten card
-   // make grid
 
    // +_+_+_+_+_+_+_+_+_+  CREATE CARDS +_+_+_+_+_+_+_+_+_+_+_+_+_+_+
    //create card
-   let cards_wrapper = document.getElementById("cards-wrapper");
-   let card = document.createElement("div");
-   card.setAttribute("class", "card");
-   cards_wrapper.appendChild(card);
+   // =-=-=-=-== TESTING FOR LOOP =-=-=-=-=-=-=
+   for (let i = 0; i < 3; i++) {
+      let cards_wrapper = document.getElementById("cards-wrapper");
+      let card = document.createElement("div");
+      card.setAttribute("class", "card");
+      cards_wrapper.appendChild(card);
 
-   // create image
-   let employee_image = document.createElement("img");
-   employee_image.setAttribute("src", employeeData[0].image);
-   employee_image.setAttribute("class", "employee-image");
-   card.appendChild(employee_image);
+      // create image
+      let employee_image = document.createElement("img");
+      employee_image.setAttribute("src", employeeData[i].image);
+      employee_image.setAttribute("class", "employee-image");
+      card.appendChild(employee_image);
 
-   //create name
-   let employee_name = document.createElement("p");
-   employee_name.setAttribute("class", "employee-name");
-   employee_name.innerHTML =
-      employeeData[0].first_name + " " + employeeData[0].last_name;
-   card.appendChild(employee_name);
+      //create name
+      let employee_name = document.createElement("span");
+      employee_name.setAttribute("class", "employee-name");
+      employee_name.innerHTML =
+         employeeData[i].first_name + " " + employeeData[i].last_name;
+      card.appendChild(employee_name);
 
-   // create email
-   let employee_email = document.createElement("p");
-   employee_email.setAttribute("class", "employee-email");
-   employee_email.innerHTML = employeeData[0].email;
-   card.appendChild(employee_email);
+      // create email
+      let employee_email = document.createElement("span");
+      employee_email.setAttribute("class", "employee-email");
+      employee_email.innerHTML = employeeData[i].email;
+      card.appendChild(employee_email);
 
-   //create city
-   let employee_city = document.createElement("p");
-   employee_city.setAttribute("class", "employee-city");
-   employee_city.innerHTML = employeeData[0].city;
-   card.appendChild(employee_city);
+      //create city
+      let employee_city = document.createElement("span");
+      employee_city.setAttribute("class", "employee-city");
+      employee_city.innerHTML = employeeData[i].city;
+      card.appendChild(employee_city);
+   }
    // +_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+
+}
+
+function getAPIdata() {
+   // TODO here is where we will get the JSON stuff
 }
 
 function main() {
    // This comment serves no purpose
    let disclaimer = "This is the JavaScript for Treehouse Project 8";
    console.log(disclaimer);
+   // FIXME This will be
+   // TODO const api = getAPIdata();
+   getAPIdata();
+   // TODO and this will be
+   // FIXME showUsers(api);
    showUsers();
 }
 
